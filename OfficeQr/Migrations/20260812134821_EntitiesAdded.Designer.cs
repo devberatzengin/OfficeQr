1915e4f383b4de86094e1342953dc7b70629c79d
Berat Zengin <devberatzengin@gmail.com>
2026-08-12 16:57:40 +0300

    feat: Item, Shelf and Cabiet Migratiomed to db

--- OfficeQr/Migrations/20260812134821_EntitiesAdded.Designer.cs
+++ OfficeQr/Migrations/20260812134821_EntitiesAdded.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using OfficeQr.Data;
@@ -11,9 +12,11 @@ using OfficeQr.Data;
 namespace OfficeQr.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260812134821_EntitiesAdded")]
+    partial class EntitiesAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ OfficeQr/Migrations/20260812134821_EntitiesAdded.cs
@@ -0,0 +1,170 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OfficeQr.Migrations
+{
+    /// <inheritdoc />
+    public partial class EntitiesAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Items_AspNetUsers_UserId",
+                schema: "identity",
+                table: "Items");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Items_Cabinets_CabinetId",
+                schema: "identity",
+                table: "Items");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Items_Shelves_ShelfId",
+                schema: "identity",
+                table: "Items");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Shelves_Cabinets_CabinetId",
+                schema: "identity",
+                table: "Shelves");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Shelves_QrCode",
+                schema: "identity",
+                table: "Shelves",
+                column: "QrCode",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Items_QrCode",
+                schema: "identity",
+                table: "Items",
+                column: "QrCode",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cabinets_QrCode",
+                schema: "identity",
+                table: "Cabinets",
+                column: "QrCode",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Items_AspNetUsers_UserId",
+                schema: "identity",
+                table: "Items",
+                column: "UserId",
+                principalSchema: "identity",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Items_Cabinets_CabinetId",
+                schema: "identity",
+                table: "Items",
+                column: "CabinetId",
+                principalSchema: "identity",
+                principalTable: "Cabinets",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Items_Shelves_ShelfId",
+                schema: "identity",
+                table: "Items",
+                column: "ShelfId",
+                principalSchema: "identity",
+                principalTable: "Shelves",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Shelves_Cabinets_CabinetId",
+                schema: "identity",
+                table: "Shelves",
+                column: "CabinetId",
+                principalSchema: "identity",
+                principalTable: "Cabinets",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Items_AspNetUsers_UserId",
+                schema: "identity",
+                table: "Items");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Items_Cabinets_CabinetId",
+                schema: "identity",
+                table: "Items");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Items_Shelves_ShelfId",
+                schema: "identity",
+                table: "Items");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Shelves_Cabinets_CabinetId",
+                schema: "identity",
+                table: "Shelves");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Shelves_QrCode",
+                schema: "identity",
+                table: "Shelves");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Items_QrCode",
+                schema: "identity",
+                table: "Items");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Cabinets_QrCode",
+                schema: "identity",
+                table: "Cabinets");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Items_AspNetUsers_UserId",
+                schema: "identity",
+                table: "Items",
+                column: "UserId",
+                principalSchema: "identity",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Items_Cabinets_CabinetId",
+                schema: "identity",
+                table: "Items",
+                column: "CabinetId",
+                principalSchema: "identity",
+                principalTable: "Cabinets",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Items_Shelves_ShelfId",
+                schema: "identity",
+                table: "Items",
+                column: "ShelfId",
+                principalSchema: "identity",
+                principalTable: "Shelves",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Shelves_Cabinets_CabinetId",
+                schema: "identity",
+                table: "Shelves",
+                column: "CabinetId",
+                principalSchema: "identity",
+                principalTable: "Cabinets",
+                principalColumn: "Id");
+        }
+    }
+}
