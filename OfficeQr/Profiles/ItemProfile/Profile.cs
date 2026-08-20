172faa1630b67ad1bd542cbcb5b6366d84227891
Berat Zengin <devberatzengin@gmail.com>
2026-08-20 11:51:56 +0300

    fix: fix status property settings in mapper

--- OfficeQr/Profiles/ItemProfile/Profile.cs
+++ OfficeQr/Profiles/ItemProfile/Profile.cs
@@ -14,6 +14,7 @@ public class ItemProfile : Profile
             .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
             .ForMember(dest => dest.QrCode, opt => opt.MapFrom(src => src.QrCode))
             .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+            .ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.Status.ToString()))
             .ForMember(dest => dest.UserId, opt => opt.MapFrom(src => src.UserId))
             .ForMember(dest => dest.ShelfId, opt => opt.MapFrom(src => src.ShelfId)); 
         
